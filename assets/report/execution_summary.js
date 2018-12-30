841ec26d4dd4c5e3a90ed0b82590856ca5b5f3d0
automike <mikeliucc@users.noreply.github.com>
2018-12-30 01:12:20 -0800

    update static web content for execution report.
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- assets/report/execution_summary.js
+++ assets/report/execution_summary.js
@@ -1,31 +1,12 @@
-/*
- * Copyright 2012-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 "use strict";
 
-function findTargetId(/*Event*/event) { return $(event.target.parentNode).attr('target'); }
-
-function showSection() {
-    let sectionId = findTargetId(event);
+function showSection(/*HTMLElement*/icon) {
+    let sectionId = $(icon.parentNode).attr('target');
     if (sectionId) { $('#' + sectionId).show(250); }
 }
 
-function hideSection() {
-    console.log(event);
-    let sectionId = findTargetId(event);
+function hideSection(/*HTMLElement*/icon) {
+    let sectionId = $(icon.parentNode).attr('target');
     if (sectionId) { $('#' + sectionId).hide(250); }
 }
 
@@ -36,3 +17,7 @@ function showAllSections() {
 function hideAllSections() {
     $('.showhide').each(function (index, elem) { $('#' + $(elem).attr('target')).hide(250); });
 }
+
+function collapsePassRows() { $('tr.noFailure').css('visibility', 'collapse'); }
+
+function showPassRows() { $('tr.noFailure').css('visibility', 'visible'); }
